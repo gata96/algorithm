8458f72b683e3dfb41608cf3311fd7dda2e15e2f
Gata <121420427+gata96@users.noreply.github.com>
2023-01-28 14:26:20 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+num = list(map(int, input().split()))
+cnt = len(list(set(num)))
+if cnt == 1:
+    print(10000 + num[0]*1000)
+
+elif cnt == 3:
+    print(max(num)*100)
+
+else:
+    num.remove(max(num))
+    num.remove(min(num))
+    print(1000 + num[0]* 100)
