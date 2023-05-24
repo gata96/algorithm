b97376f7f654b530e0d72f82834a4f9c15478f68
gata96 <121420427+gata96@users.noreply.github.com>
2023-05-24 19:30:21 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,10 +1,10 @@
 def solution(a, b, n):
     cnt = 0
-    while (n >= a):
-        namoji = n % a
-        n = (n // a) * b
-        cnt += n
-        n += namoji
+    while (n >= a): # 콜라가 2개 이상인 동안 반복 수행
+        namoji = n % a # 나머지 빈병
+        n = (n // a) * b # 받을 수 있는 빈병
+        cnt += n # 받은 병 카운트
+        n += namoji # 나머지 빈병을 다시 더해줌
     return cnt
         
         
